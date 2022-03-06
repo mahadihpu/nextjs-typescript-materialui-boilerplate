da2d35599fea791d90eff2ff2b6eaa2bca6a031b
mahadiinsta <mahadi@instawebworks.com.au>
2022-03-06 11:39:49 +0600

    Final Clearing

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,5 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
+import React, { ReactNode } from 'react'
 
 type Props = {
   children?: ReactNode
@@ -15,20 +14,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
+      <nav></nav>
     </header>
     {children}
     <footer>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <h1>Next.js || TypeScript || Material UI V.5.4 --- BoilerPlate 👋</h1>
   </Layout>
 )
 
